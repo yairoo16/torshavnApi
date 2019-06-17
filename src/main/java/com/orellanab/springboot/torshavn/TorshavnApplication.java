c09252b05903d26a3f5ae0ad5ef92122e8af109f
Benyair Orellana <ben.orellana@gmail.com>
2019-06-17 10:19:19 -0400

    initial commit

---
+++ src/main/java/com/orellanab/springboot/torshavn/TorshavnApplication.java
@@ -0,0 +1,13 @@
+package com.orellanab.springboot.torshavn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorshavnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorshavnApplication.class, args);
+	}
+
+}

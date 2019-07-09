1bb30443235f04fc9393cedaae47b7702351296e
yairoo16 <Junglist08>
2019-07-08 20:53:21 -0400

    web security setup

---
+++ src/main/java/com/orellanab/springboot/torshavn/entity/User.java
@@ -0,0 +1,56 @@
+package com.orellanab.springboot.torshavn.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="user", schema="public")
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 110733066883834909L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column
+	private String username;
+	@Column
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+
+}
---
+++ src/main/java/com/orellanab/springboot/torshavn/service/UserService.java
@@ -0,0 +1,70 @@
+package com.orellanab.springboot.torshavn.service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.orellanab.springboot.torshavn.entity.User;
+import com.orellanab.springboot.torshavn.repo.interfaces.UserRepo;
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	private UserRepo userRepo;
+
+	// private PasswordEncoder bcryptEncoder;
+	
+	@Autowired
+	public UserService(UserRepo userRepo
+			//, PasswordEncoder bcryptEncoder
+					   ) {
+
+		this.userRepo = userRepo;
+		// this.bcryptEncoder = bcryptEncoder;
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = this.userRepo.loadUserByUsername(username);
+
+		if (user != null) {
+
+//			Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+//			grantedAuthorities.add(new SimpleGrantedAuthority(user.getUsername()));
+//
+//			return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+
+			if ("test123".equals(username)) {
+				return new
+						org.springframework.security.core.userdetails.User("test123", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+						new ArrayList<>());
+			} else {
+				throw new UsernameNotFoundException("User not found with username: " + username);
+			}
+
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+
+	}
+
+	public User saveUser(User user) {
+
+		return this.userRepo.saveUser(user);
+	}
+
+
+
+}
